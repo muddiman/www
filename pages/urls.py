a3e3abdde199e45dbd2e72f9255a94d0d3c78b68
Roger Clarke <rogerclarke00@hotmail.com>
2018-11-08 17:28:53 -0400

    links

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from pages import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^contact', views.contact, name='contact'),
+    url(r'^projects', views.projects, name='projects'),
+    url(r'^about', views.about, name='about'),
+]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "pages/index.html", {})
+
+def contact(request):
+    return render(request, "pages/contact.html", {})
+
+def projects(request):
+    return render(request, "pages/projects.html", {})
+
+def about(request):
+    return render(request, "pages/about.html", {})
