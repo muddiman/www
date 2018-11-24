f92bdde4fa23065bfe44fe2b73210145ff5fc9e8
Roger Clarke <rogerclarke00@hotmail.com>
2018-11-23 21:41:57 -0400

    posts in admin page

--- blog/views.py
+++ blog/views.py
@@ -3,24 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Post
-# Create your views here.
 
-"""
-posts = [
-    {
-        'author': '.muddicode',
-        'title': 'First Blog Post',
-        'content': 'This is a test.',
-        'date_posted': 'November 22, 2018'
-    },
-    {
-        'author': 'Roger A. Clarke',
-        'title': '2nd Blog Post',
-        'content': 'This is also a test.',
-        'date_posted': 'December 25, 2018'
-    }
-]
-"""
 
 def home(request):
     context = {
