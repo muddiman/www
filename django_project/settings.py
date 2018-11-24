72dab248115a37c7b4884d2a531a415962a15e59
Roger Clarke <rogerclarke00@hotmail.com>
2018-11-23 22:36:33 -0400

    .

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = "/home/django/django_project/static"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR, "static"),
+# ]
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # Allow Django from all hosts. This snippet is installed from
 # /var/lib/digitalocean/allow_hosts.py
