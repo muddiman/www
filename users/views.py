a0bd28916a1ade37cadf0a2a39bab0e3ed4fed3c
Roger Clarke <rogerclarke00@hotmail.com>
2018-11-26 18:34:31 -0400

    message validation

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
-	form = UserCreationForm()
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			messages.success(request, 'Account created for '+ {username}+'!')
+			return redirect('pages-landing-page')
+	else:
+		form = UserCreationForm()
 	return render(request, 'users/register.html', {'form': form})
