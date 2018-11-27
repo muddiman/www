893f66172331e47702cdf50af4feced5b6c8c753
Roger Clarke <rogerclarke00@hotmail.com>
2018-11-26 23:09:01 -0400

    # test

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
 			form.save()
-			username = form.cleaned_data.get('username')
+#			username = form.cleaned_data.get('username')
 			messages.success(request, 'Your account was successfully created. Please login now!')
 			return redirect('login')
 	else:
